bed654e45863206e6127ee0280ef0087bb6337b9
cishocksr <cishockleysr@gmail.com>
2019-10-15 18:07:22 -0400

    Character list rendered

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,12 @@
 import React from "react";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+export default function CharacterCard(props) {
+  return (
+    <div key={props.id}>
+      <h1>{props.name}</h1>
+      <img src={props.image} alt={props.name} />
+      <p>{props.species}</p>
+      <p>{props.status}</p>
+    </div>
+  );
 }
